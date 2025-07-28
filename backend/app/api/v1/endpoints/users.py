d5f9dff60b386c13426c5c8509c58aeb1a2225c2
gautamborah <gautam.borah@gmail.com>
2025-07-27 18:43:57 -0700

    user creation authorization added

--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -8,9 +8,9 @@ from app.services.user_service import list_users, create_user, get_user, update_
 
 from app.auth.session import get_session_user
 
-router = APIRouter()
+router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/users/", response_model=List[User])
+@router.get("/", response_model=List[User])
 def read_users_route(request: Request):
     sessionUser = get_session_user(request)
     role = sessionUser.role
@@ -18,14 +18,19 @@ def read_users_route(request: Request):
         raise HTTPException(status_code=403, detail="Admins only")
     return list_users()
 
-@router.post("/users/", response_model=User)
-def create_user_route(user: User):
+@router.post("/", response_model=User)
+def create_user_route(request: Request, user: User):
+    sessionUser = get_session_user(request)
+    # check if the role is admin
+    if sessionUser.role != Role.admin:
+        raise HTTPException(status_code=403, detail="Admins only")
+
     user_data = user.dict()
     user_data["role"] = str(user.role.value)
 
     return create_user(user)
 
-@router.get("/users/{user_id}", response_model=User)
+@router.get("/{user_id}", response_model=User)
 def get_user_route(request: Request, user_id: str):
     sessionUser = get_session_user(request)
     if sessionUser.user_id != user_id:
@@ -34,6 +39,6 @@ def get_user_route(request: Request, user_id: str):
     return get_user(user_id)
 
 
-@router.put("/users/{user_id}", response_model=User)
+@router.put("/{user_id}", response_model=User)
 def update_user(email: str, user: User):
     return update_user(user)
