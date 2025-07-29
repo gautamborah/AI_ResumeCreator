8a5e2c4cb1ae46e87f30a848f2a390fcf3548786
gautamborah <gautam.borah@gmail.com>
2025-07-28 22:16:08 -0700

    authorize_user_or_admin implemented

--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -1,44 +1,38 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, Depends
 from pydantic import BaseModel, EmailStr
 from typing import List
 
 from app.models.user import User
 from app.models.role import Role
+from app.models.sessionUser import SessionUser
 from app.services.user_service import list_users, create_user, get_user, update_user
 
-from app.auth.session import get_session_user
+from app.auth.session import get_session_user, require_admin, authorize_user_or_admin
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+
+
 @router.get("/", response_model=List[User])
-def read_users_route(request: Request):
-    sessionUser = get_session_user(request)
-    role = sessionUser.role
-    if role != Role.admin:
-        raise HTTPException(status_code=403, detail="Admins only")
+def read_users_route(_: SessionUser = Depends(require_admin)):
     return list_users()
 
 @router.post("/", response_model=User)
-def create_user_route(request: Request, user: User):
-    sessionUser = get_session_user(request)
-    # check if the role is admin
-    if sessionUser.role != Role.admin:
-        raise HTTPException(status_code=403, detail="Admins only")
-
-    user_data = user.dict()
-    user_data["role"] = str(user.role.value)
-
+def create_user_route(user: User, _: SessionUser = Depends(require_admin)):
     return create_user(user)
 
-@router.get("/{user_id}", response_model=User)
-def get_user_route(request: Request, user_id: str):
-    sessionUser = get_session_user(request)
-    if sessionUser.user_id != user_id:
-        raise HTTPException(status_code=403, detail="User not matching")
 
+@router.get("/{user_id}", response_model=User)
+def get_user_route(
+    user_id: str,
+    _: SessionUser = Depends(authorize_user_or_admin())
+):
     return get_user(user_id)
 
-
 @router.put("/{user_id}", response_model=User)
-def update_user(email: str, user: User):
+def update_user_route(
+    user_id: str,
+    user: User,
+    _: SessionUser = Depends(authorize_user_or_admin())
+):
     return update_user(user)
--- backend/app/auth/session.py
+++ backend/app/auth/session.py
@@ -1,7 +1,7 @@
 from itsdangerous import URLSafeTimedSerializer, BadSignature
 from app.models.role import Role
 from app.models.sessionUser import SessionUser
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, Depends, Path
 
 SECRET_KEY = "3sCzxaz_5tG4lqhv7gkwgdzFlKbxqHESDoo-LvUEXxQ"
 SESSION_COOKIE_NAME = "resume_session"
@@ -23,3 +23,21 @@ def get_session_user(request: Request) -> SessionUser:
         return SessionUser(user_id=user_id, role=role)
     except BadSignature:
         raise HTTPException(status_code=401, detail="Invalid session token")
+
+
+def authorize_user_or_admin() -> SessionUser:
+    def dependency(
+        user_id: str = Path(...),
+        current_user: SessionUser = Depends(get_session_user)
+    ) -> SessionUser:
+        if current_user.role == Role.admin:
+            return current_user
+        if current_user.user_id != user_id:
+            raise HTTPException(status_code=403, detail="Forbidden")
+        return current_user
+    return dependency
+
+def require_admin(user: SessionUser = Depends(get_session_user)):
+    if user.role != Role.admin:
+        raise HTTPException(status_code=403, detail="Admins only")
+    return user
